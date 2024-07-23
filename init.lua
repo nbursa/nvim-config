8e6a2fcd5c4d2c139276b0bbfaabc395a5cf859d
Nenad <nbursa@gmail.com>
2024-07-23 17:10:06 +0200

    Fix ignore

--- init.lua
+++ init.lua
@@ -9,9 +9,33 @@ vim.wo.relativenumber = true
 -- Enable mouse support
 vim.opt.mouse = 'a'
 
--- Load plugin configurations
-require('config.plugins')
+vim.api.nvim_exec([[
+  augroup BufferlineCustomCSS
+    autocmd!
+    autocmd VimEnter * call AddBufferlineCSS()
+  augroup END
+
+  function! AddBufferlineCSS() abort
+    if exists(':hi')
+      highlight BufferlineBufferIconSelected guibg=#1e1e1e gui=bold guifg=#ffffff
+      highlight BufferlineBufferCloseIcon guibg=#1e1e1e guifg=#ffffff
+      highlight BufferlineBufferModifiedIcon guibg=#1e1e1e guifg=#ffffff
+    endif
+  endfunction
+]], false)
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0) == "" then
+      vim.cmd("quit")
+    end
+  end,
+})
 
 -- Load custom keybindings
 require('config.keybindings')
+
+-- Load plugin configurations
+require('config.plugins')
 
