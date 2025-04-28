4310c984949f0e8498bb91ad5824465a057f82f6
Nenad <nbursa@gmail.com>
2025-04-28 15:39:38 +0200

    chore: cleanup

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -1,13 +1,19 @@
 require'nvim-tree'.setup {
-    disable_netrw = true,
-    hijack_netrw = true,
-    actions = {
-      open_file = {
-        quit_on_open = true,
-      },
+  disable_netrw = true,
+  hijack_netrw = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+  actions = {
+    open_file = {
+      quit_on_open = true,
     },
-  }
- 
-  vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = function()
+  },
+}
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { 
+  callback = function()
     require("nvim-tree.api").tree.open()
-  end })
+  end 
+})
